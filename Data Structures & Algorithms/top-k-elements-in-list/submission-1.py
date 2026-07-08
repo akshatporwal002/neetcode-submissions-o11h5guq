1f6112f431872d55df584b2b5b54cd5bde3f1d18
Akshat Porwal <125797594+akshatporwal002@users.noreply.github.com>
2026-07-08 14:27:14 +1000

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,36 @@
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # use bucketsort
+        counts = {}
+        for i in nums:
+            if i not in counts:
+                counts[i] = 1 
+            else:
+                counts[i] += 1 
+        
+        freq = {}
+        maxfreq = 0
+        for i in counts:
+            f = counts[i]
+            if f not in freq:
+                freq[f] = [i]
+            else:
+                freq[f].append(i)
+            
+            if f > maxfreq:
+                maxfreq = f
+        
+        vals = []
+        while k > 0:
+            while maxfreq not in freq:
+                maxfreq -= 1
+            vals.extend(freq[maxfreq])
+            
+            k -= len(freq[maxfreq])
+            maxfreq -=1
+        # we do not need to check if last bucket has more 
+        #elemants than necessary as guarenteed unique solution
+        return vals
+            
+        
+
